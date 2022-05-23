a6baeb7919156d26a47d99a6163883af8408dc2f
kayleeburch <kayleemburch@gmail.com>
2022-05-23 19:46:58 -0400

    final solution day 1

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,20 @@
-function deafGrandma() {
-
+function deafGrandma(count = 0) {
+    let input = prompt('HEY KID!')
+    if(input === ''){
+        alert('WHAT?!');
+    } else if(input === "GOODBYE!" && count === 0){
+        alert('LEAVING SO SOON?')
+        return deafGrandma(1);
+    } else if(input === "GOODBYE!" && count === 1){
+        alert('LATER, SKATER!')
+        return;
+    } else if(input === input.toUpperCase()){
+        alert('NO, NOT SINCE 1946!');
+    } else {
+        alert('SPEAK UP, KID!');
+    }
+     deafGrandma(count)
 }
 
 deafGrandma();
+
